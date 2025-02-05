688f5ecb8c580d1d1e025e0685edf7ab6b95796f
jonathansaboury <130409862+jonathansaboury@users.noreply.github.com>
2025-02-04 16:33:50 -0800

    Update vmunivprint02_zonal_failover.ps1

--- vmunivprint02_zonal_failover.ps1
+++ vmunivprint02_zonal_failover.ps1
@@ -8,7 +8,7 @@ $OSDiskName = "dsk-vmunivprint02-os-pssd-westus2-zrs"
 $NICName = "nic-vmunivprint02-prod-001"
 
 $LocationName = "westus2"
-$VMSize_Zone1 = "Standard_D4as_v5"
+$VMSize = "Standard_D4as_v5"
 
 #Step 1: Delete only VM.
 Remove-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Force
@@ -17,7 +17,7 @@ Remove-AzVM -ResourceGroupName $ResourceGroupName -Name $VMName -Force
 $OSDisk =  Get-AzDisk -DiskName $OSDiskName -ResourceGroupName $ResourceGroupName
 $NIC = Get-AzNetworkInterface -ResourceGroupName $ResourceGroupName -Name $NICName
 
-$VirtualMachineConfig = New-AzVMConfig -VMName $VMName -VMSize $VMSize_Zone1 -EncryptionAtHost -Zone @($Zone)# #Zone input is an array.
+$VirtualMachineConfig = New-AzVMConfig -VMName $VMName -VMSize $VMSize -EncryptionAtHost -Zone @($Zone)# #Zone input is an array.
 $VirtualMachineConfig = Add-AzVMNetworkInterface -VM $VirtualMachineConfig -Id $NIC.Id -DeleteOption "Detach"
 $VirtualMachineConfig = Set-AzVMOSDisk -VM $VirtualMachineConfig -ManagedDiskId $OSDisk.Id -DiskSizeInGB 128 -CreateOption Attach -Windows -DeleteOption Detach -StorageAccountType $OSDisk.Sku.Name -Name $OSDisk.Name
 $VirtualMachineConfig = Set-AzVMBootDiagnostic -VM $VirtualMachineConfig -Enable -ResourceGroupName $ResourceGroupName -StorageAccountName "stvmunivprint02bootdiags" 
